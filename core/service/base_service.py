d4eaac03165530f975e7cb5cac3d101527f9de30
zuongthao <zuongthao.tn@gmail.com>
2025-12-23 14:12:28 +0700

    update core service & broker head

--- core/service/base_service.py
+++ core/service/base_service.py
@@ -19,24 +19,12 @@ class BaseServicePlugin:
         pass
 
     def trigger_before(self, data=None, strategy_name=""):
-        if isinstance(data, pd.DataFrame):
-            last = data.iloc[-1]
-            payload = {
-                "index": last.name.isoformat(),
-                "values": last.to_dict()
-            }
-        else:
-            payload = data
-        log_data = {
-            "timestamp": datetime.now().isoformat(),
-            "service_name": self.name,
-            "trigger_name": self.name + ".trigger_before",
-            "strategy_name": strategy_name,
-            "payload": payload
-        }
-        self.log_queue.log(log_data)
+        self.trigger(data=data, strategy_name=strategy_name, trigger_name_postfix="before")
 
     def trigger_after(self, data=None, strategy_name=""):
+        self.trigger(data=data, strategy_name=strategy_name, trigger_name_postfix="after")
+
+    def trigger(self, data=None, strategy_name="", trigger_name_postfix=""):
         if isinstance(data, pd.DataFrame):
             last = data.iloc[-1]
             payload = {
@@ -48,7 +36,7 @@ class BaseServicePlugin:
         log_data = {
             "timestamp": datetime.now().isoformat(),
             "service_name": self.name,
-            "trigger_name": self.name + ".trigger_after",
+            "trigger_name": self.name + ".trigger_" + trigger_name_postfix,
             "strategy_name": strategy_name,
             "payload": payload
         }
